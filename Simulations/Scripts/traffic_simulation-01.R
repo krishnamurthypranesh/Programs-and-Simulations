fbf395d0bc1fe9048e34f4be7e510c69a446f230
krishnamurthypranesh <pkstockac@gmail.com>
2018-03-19 22:10:58 +0530

    changed arg name in traf_sim one

--- Simulations/Scripts/traffic_simulation-01.R
+++ Simulations/Scripts/traffic_simulation-01.R
@@ -22,15 +22,15 @@ get_speed <- function(object) {
 # get_distance ------------------------------------------------------------
 
 
-get_distance <- function(time, object = create_vehicle(),
+get_distance <- function(time_sim, object = create_vehicle(),
                          speed = get_speed(object)) {
   
-  stopifnot(is.numeric(time), is.numeric(speed), length(speed) == 1, 
+  stopifnot(is.numeric(time_sim), is.numeric(speed), length(speed) == 1, 
             is.character(object), length(object) == 1)
   distance_covered <- 0
   i <- 1
   
-  while (i <= length(time)) {
+  while (i <= length(time_sim)) {
     distance_covered <- distance_covered + speed
     i <- i + 1
   }
@@ -40,17 +40,17 @@ get_distance <- function(time, object = create_vehicle(),
 
 # increase_distance -------------------------------------------------------
 
-increase_distance <- function(time, object = create_vehicle(),
+increase_distance <- function(time_sim, object = create_vehicle(),
                               speed = get_speed(object)) {
-  stopifnot(is.numeric(time), is.numeric(speed), length(speed) == 1, 
+  stopifnot(is.numeric(time_sim), is.numeric(speed), length(speed) == 1, 
             is.character(object), length(object) == 1)
   
-  distances <- vector("numeric", length(time))
+  distances <- vector("numeric", length(time_sim))
   distances[[1]] <- 0
-  time <- time[order(time)]
+  time_sim <- time_sim[order(time_sim)]
   i <- 2
   
-  while (i <= length(time)) {
+  while (i <= length(time_sim)) {
     distances[[i]] <- distances[[i - 1]] + speed
     i <- i + 1
   }
@@ -103,16 +103,16 @@ distance_reduce <- function(time_sim, object = create_vehicle(),
 
 # sim_traffic -------------------------------------------------------------
 
-sim_traffic <- function(time) {
-  stopifnot(is.numeric(time))
+sim_traffic <- function(time_sim) {
+  stopifnot(is.numeric(time_sim))
   
-  vehicles <- vector("character", length(time))
-  init_speed <- vector("numeric", length(time))
-  distance_covered <- vector("numeric",length(time))
+  vehicles <- vector("character", length(time_sim))
+  init_speed <- vector("numeric", length(time_sim))
+  distance_covered <- vector("numeric",length(time_sim))
   
-  reduced_speeds <- vector("numeric", length(time))
+  reduced_speeds <- vector("numeric", length(time_sim))
   
-  for (i in 1:length(time)) {
+  for (i in 1:length(time_sim)) {
     vehicles[[i]] <- create_vehicle()
     init_speed[[i]] <- get_speed(vehicles[[i]])
     reduced_speeds[[i]] <- reduce_speed(vehicles[[i]],
@@ -120,16 +120,18 @@ sim_traffic <- function(time) {
     if (init_speed[[i]] != reduced_speeds[[i]]) {
       distance_covered[[i]] <- purrr::map2_dbl(.x = vehicles[[i]],
                                                .y = init_speed[[i]],
-                                               get_distance, time = time)
+                                               get_distance, 
+                                               time_sim = time_sim)
     }
     
     else {
       distance_covered[[i]] <- purrr::map2_dbl(.x = vehicles[[i]],
                                                .y = reduced_speeds[[i]],
-                                               get_distance, time = time)
+                                               get_distance, 
+                                               time_sim = time_sim)
     }
     
-    # distance_covered[[i]] <- distance_reduce(time, vehicles[[i]],
+    # distance_covered[[i]] <- distance_reduce(time_sim, vehicles[[i]],
     #                                          init_speed[[i]])
   }
   
