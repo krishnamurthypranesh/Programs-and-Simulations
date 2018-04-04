ccaa7d7d0d90c64b0e0d08834996f5003887fe07
krishnamurthypranesh <pkstockac@gmail.com>
2018-04-04 07:41:43 +0530

    Update properties of class vehicle

--- Simulations/Scripts/nagel_shreckenberg.R
+++ Simulations/Scripts/nagel_shreckenberg.R
@@ -2,32 +2,46 @@
 # The said model is a cellular automation model which simulates one way traffic
 # using cars.
 
-# Function to create vehicles ----------------------------------------------
+# Function to store constraints -------------------------------------------
+constraints <- function(vmin = 1, vmax = 5, distance = 100) {
+  (constraints <- list(vmin = vmin, vmax = vmax, distance = distance))
+}
 
+const_list <- constraints()
+
+# Function to create vehicles ----------------------------------------------
 create_vehicle <- function(name, size = 3) {
-  stopifnot(is.character(name), length(name) == 1)
-  
-  vehicle <- structure(list(name = name,
-                            size = size,
-                            speed = sample(1:5, size = 1)), 
-                       class = "vehicle")
+  vehicle <- vehicle(name, size, const_list$vmin, const_list$vmax)
   
   vehicle
 }
 
-car1 <- create_vehicle("car")
+#=====----------==============---------------================--------
+# car1 <- create_vehicle("car")
+# 
+# car1$name
+# 
+# cars <- c("mustang", "ragera", "chiron", "veyron", "gallardo", "gtr", 
+#           "berlinetta", "phantom", "huracan", "sesto elemento")
+# 
+# vapply(cars, create_vehicle, FUN.VALUE = c(list(name = "car", size = 0,
+#                                                 speed = 0)))
+# 
+# cars_list <- lapply(cars, create_vehicle)
 
-car1$name
 
-cars <- c("mustang", "ragera", "chiron", "veyron", "gallardo", "gtr", 
-          "berlinetta", "phantom", "huracan", "sesto elemento")
-
-vapply(cars, create_vehicle, FUN.VALUE = c(list(name = "car", size = 0,
-                                                speed = 0)))
-
+# Function to increase speed ----------------------------------------------
+increase_speed <- function(vehicle) {
+  stopifnot(class(vehicle) == "vehicle")
+  
+  if (vehicle$speed < const_list$vmax) {
+    vehicle$speed <- vehicle$speed + 1
+  }
+  
+  vehicle$speed
+}
 
 # Function to reduce speed ------------------------------------------------
-
 reduce_speed <- function(vehicle1, vehicle2) {
   speed1 <- vehicle1$speed
   speed2 <- vehicle2$speed
@@ -43,8 +57,19 @@ reduce_speed <- function(vehicle1, vehicle2) {
 }
 
 
-# Function to get distance travelled --------------------------------------
+# Function to randomly reduce speed ---------------------------------------
 
+rand_speed_reduce <- function(vehicle, const_list = const_list, ...) {
+  logi_val <- sample(c(TRUE, FALSE), size = 1)
+  
+  if (isTRUE(logi_val) && vehicle$speed > const_list$vmin) {
+    vehicle$speed <- vehicle$speed - 1
+  }
+  
+  vehicle$speed
+}
+
+# Function to get distance travelled --------------------------------------
 distance_travelled <- function(vehicle, time_duration) {
   time <- seq(time_duration)
   distance <- 0
@@ -56,8 +81,8 @@ distance_travelled <- function(vehicle, time_duration) {
   
   distance
 }
-# Function to check distance between two cars -----------------------------
 
+# Function to check distance between two cars -----------------------------
 red_speed <- function(vehicle1, vehicle2) {
   distance_car1 <- distance_travelled(car1)
   
@@ -72,17 +97,29 @@ red_speed <- function(vehicle1, vehicle2) {
     reduce_speed(vehicle1, vehicle2)
   }
 }
-# Function to simulate vehicles ---------------------------------------------
 
-sim_traffic <- function(length_sim = 10) {
-  cars_list <- lapply(rep("car", length_sim), create_vehicle)
+# Function to simulate vehicles ---------------------------------------------
+sim_traffic <- function(length_sim = 10, ...) {
+  cars <- c("mustang", "ragera", "chiron", "veyron", "gallardo", "gtr", 
+            "berlinetta", "phantom", "huracan", "sesto elemento")
+  
+  cars_list <- lapply(sample(cars, size = length_sim, replace = TRUE), 
+                      create_vehicle)
+  names(cars_list) <- sample(cars, size = length_sim, replace = T)
   
   # getting distance travelled 
   distances <- vapply(cars_list, distance_travelled, 
                       time_duration = length_sim, FUN.VALUE = c(distance = 0))
+  # increasing speeds
+  increased_speeds <- vapply(cars_list, increase_speed, FUN.VALUE = c(x = 0))
+  #random reduction in speeds
+  reduced_speeds <- vapply(cars_list, rand_speed_reduce, FUN.VALUE = c(x = 0))
+  vehicle_stats <- list(vehicles = cars_list, 
+                        distances = distances, 
+                        final_speeds = increased_speeds,
+                        random_speed_reductions = reduced_speeds)
   
-  (sim_data <- data.frame(vehicles = as.data.frame(cars_list),
-                          distance = distances))
+  vehicle_stats
 }
 
 sim_traffic()
--- Simulations/Scripts/vehicle_prep.R
+++ Simulations/Scripts/vehicle_prep.R
@@ -1,19 +1,43 @@
 
 # Constructor function ----------------------------------------
 
-vehicle <- function(name, size, vmin = 1, vmax = 5) {
-  stopifnot(is.character(name), length(name) == 1, 
+vehicle <- function(type, size, vmin, vmax) {
+  stopifnot(is.character(type), length(type) == 1, 
             is.numeric(size), length(size) == 1)
   
   speed_vector <- seq(vmin, vmax)
-  vehicle <- structure(list(name = name,
+  vehicle <- structure(list(type = type,
                             size = size,
-                            speed = sample(speed_vector, 1)))
+                            speed = sample(speed_vector, size = 1)),
+                       class = "vehicle")
   
   vehicle
 }
 
+# Function to get element by name -----------------------------------------
 
-# data.frame method for class vehicle -------------------------------------
+extractor <- function(x, element_name) {
+  stopifnot(!is.null(names(x)), is.character(element_name), 
+            length(element_name) == 1) 
+  
+  x[[element_name]]
+}
 
-as.data.frame.vehicle <- function() {}
+# # as.data.frame method for class vehicle -------------------------------------
+# 
+# get_data.frame <- function(vehicle, ...) {
+#   if(is.data.frame(vehicle)) {
+#     return(vehicle)
+#   } else if (!is.list(vehicle)) {
+#     vehicle <- as.list(vehicle)
+#   }
+#   
+#   names <- vapply(vehicle, extractor, element_name = "name",
+#                   FUN.VALUE = c(name = "a"))
+#   sizes <- vapply(vehicle, extractor, element_name = "size",
+#                   FUN.VALUE = c(size = 0))
+#   speeds <- vapply(vehicle, extractor, element_name = "speed",
+#                    FUN.VALUE = c(speed = 0))
+#   
+#   (vehicle_df <- data.frame(name = names, size = sizes, speed = speeds))
+# }
